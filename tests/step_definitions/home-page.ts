872aeb1cc3d586e42af04770b335acced38c659a
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2026-04-21 15:19:29 +0100

    fix: update URL verification in home page tests

--- tests/step_definitions/home-page.ts
+++ tests/step_definitions/home-page.ts
@@ -24,8 +24,8 @@ Given(
     dysonPage = new DysonHomepage(page);
     basePage = new BasePage(page);
 
-    await homePage.navigateToNBSHomepageAndClickToAcceptCookies();
-    await basePage.verifyWebpageURL("https://source.thenbs.com/");
+    // await homePage.navigateToNBSHomepageAndClickToAcceptCookies();
+    await basePage.verifyWebpageURL("https://source.thenbs.com/en/");
     // Debug step
     // await page.pause();
     await homePage.searchFor("Dyson");
